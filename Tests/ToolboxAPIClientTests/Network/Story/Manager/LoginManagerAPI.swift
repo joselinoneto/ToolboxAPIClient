a4d3fd4416b2832c3bebe353607dd6c5b560922d
José Neto <jneto@blissapplications.com>
2022-09-05 00:56:44 +0100

    migrate from combine to async await

---
+++ Tests/ToolboxAPIClientTests/Network/Story/Manager/LoginManagerAPI.swift
@@ -0,0 +1,28 @@
+//
+//  LoginManagerAPI.swift
+//  Astronomy Picute Today
+//
+//  Created by José Lino Neto on 03/03/2022.
+//
+
+import Foundation
+import ToolboxAPIClient
+
+public class LoginManagerAPI: LoginRequestable {
+    public func login(email: String, password: String) async throws -> LoginDto? {
+        try await BaseNetworkWorker<LoginDto>(target: LoginTarget.login(email: email, password: password)).urlRequest()
+    }
+    
+    public func createUser(email: String, password: String) async throws -> LoginDto? {
+        let login: LoginDto = LoginDto(email: email, password: password, token: nil)
+        return try await BaseNetworkWorker<LoginDto>(target: LoginTarget.createUser).urlRequest(contentBody: login)
+    }
+    
+    public func me() async throws -> UserDto? {
+        try await BaseNetworkWorker<UserDto>(target: LoginTarget.me).urlRequest()
+    }
+    
+    public init() {
+
+    }
+}
---
+++ Tests/ToolboxAPIClientTests/Network/Story/Models/UserDto.swift
@@ -0,0 +1,13 @@
+//
+//  UserDto.swift
+//  
+//
+//  Created by José Neto on 05/09/2022.
+//
+
+import Foundation
+
+public struct UserDto: Codable {
+    public let id: String
+    public let email: String
+}
